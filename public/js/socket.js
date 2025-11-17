f1398b7238cfb83baa2d3662d4d5b17a7d608dd0
Selvam B <133207410+vikeybm24@users.noreply.github.com>
2025-11-17 12:26:02 +0530

    Update signaling server URL to production

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,5 @@
 // CHANGE THIS TO YOUR BACKEND URL:
-window.SIGNALING_SERVER = "http://172.16.251.71:3000";
+window.SIGNALING_SERVER = "https://audioparty-backend.onrender.com";
 
 window.socket = io(window.SIGNALING_SERVER, {
     transports: ["websocket"]
